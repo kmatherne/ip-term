a5a796c7db2ba8304e806395064e4c82b7a3d6f2
Kaleb Matherne <kalebmatmat@gmail.com>
2023-12-28 14:33:46 -0600

    Output a basic table to the terminal

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use anyhow::Result;
 use clap::Parser;
+use serde::Deserialize;
 use std::net::{Ipv4Addr, Ipv6Addr};
+#[macro_use]
+extern crate prettytable;
 
 #[derive(Parser)]
 struct Cli {
@@ -8,25 +11,52 @@ struct Cli {
     ip_address: String,
 }
 
+#[derive(Deserialize, Debug)]
+struct IpInfo {
+    query: String,
+    status: String,
+    country: String,
+    countryCode: String,
+    region: String,
+    regionName: String,
+    city: String,
+    zip: String,
+    lat: f32,
+    lon: f32,
+    timezone: String,
+    isp: String,
+    org: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     let api_base = "http://ip-api.com/json/";
     let args = Cli::parse();
     let ip = args.ip_address;
-    println!("IP Address: {:?}", ip);
 
     if ip.parse::<Ipv4Addr>().is_ok() {
-        println!("Is an Ipv4");
     } else if ip.parse::<Ipv6Addr>().is_ok() {
-        println!("Is an Ipv6");
     } else {
         println!("Did not provide a valid IP address!");
     }
 
     let req_url = format!("{}{}", api_base, ip);
-    println!("Sending req to {:?}", req_url);
 
-    let response = reqwest::get(&req_url).await?.text().await?;
-    println!("Response: {}", response);
+    let response = reqwest::get(&req_url).await?;
+    let ip_info: IpInfo = response.json().await?;
+
+    let table = table!(
+        ["IP Info"],
+        ["IP", &ip_info.query],
+        ["Country", &ip_info.country],
+        ["Region", &ip_info.regionName],
+        ["City", &ip_info.city],
+        ["Zip", &ip_info.zip],
+        ["ISP", &ip_info.isp],
+        ["Organization", &ip_info.org]
+    );
+
+    table.printstd();
+
     Ok(())
 }
